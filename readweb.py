a51dc9649a4f8b5620fcd4247b4e83e36aae0c4f
wellsnake <wellsunweizhen@gmail.com>
2013-03-22 09:03:51 +0800

    修改了将号表存入的格式 增加了将号表结果存入数据库

--- readweb.py
+++ readweb.py
@@ -5,6 +5,7 @@ __author__ = 'well snake'
 from bs4 import BeautifulSoup as BS
 from string import strip
 import urllib2
+import sqlite3 as db
 
 #获得普通门诊号表
 response=urllib2.urlopen('http://www.chhospital.com.cn/news_show/?cid=97')
@@ -13,29 +14,33 @@ html=response.read()
 soup = BS(html)
 
 #读取门诊号表内容，整理成形如：
-#[["中医门诊",1,2,3,4,5,6,7,9],["儿科门诊",1,4,5]]
-#其中1表示星期一上午，2表示星期一下午，以此类推
+#[["中医门诊",0,1,1,1,1,1,1,1,1,1,1,1,0,0],["儿科门诊",,0,1,1,1,1,1,1,1,1,1,1,1,0,0]]
+#其中第一列表示科室名称，后面 1-开诊 0-不开诊
+#因为一周开诊分上下午，所以一共有14列分别对应不同的开诊时间
 clinic = []
 a = []
 i = 1
 for x in soup('td'):
-    #写入科室
+    #如果是科室直接写入科室
     if  x.text != u"★" and x.text != u'':
-        a.append(x.text)
+        a.append(strip(x.text))
         i = 1
-    #写入开诊日期
+    #如果当日开诊，并且不是一周最后一天
     elif x.text == u"★" and i != 14:
-        a.append(i)
+        a.append(1)
         i += 1
-    #不开诊就跳过
+    #如果当日不开诊，并且不是一周最后一天
     elif x.text == u'' and i != 14:
+        a.append(0)
         i +=1
-    #如果是最后一天就将结果写入clinic
+    #如果是最后一天并且当日开诊
     elif x.text == u"★" and i == 14:
-        a.append(i)
+        a.append(1)
         clinic.append(a)
         a = []
+    #如果是最后一天并且当日不开诊
     elif x.text == u'' and i == 14:
+        a.append(0)
         clinic.append(a)
         a = []
     else:
@@ -43,5 +48,13 @@ for x in soup('td'):
 
 #循环打印clinic
 for x in clinic:
-    print strip(x[0]),x[1:]
+    print x[0],x[1:]
+
+#将数据写入数据库
+conn = db.connect("clinic.db")
+for x in clinic:
+    sql = "INSERT INTO %s VALUES('%s',%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d)"% ("clinic_schedule_common",x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],x[12],x[13],x[14])
+    c = conn.cursor()
+    c.execute(sql)
+conn.commit()
 
