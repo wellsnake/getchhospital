c88cb9a832a046256833b633b2035a6ff6548b24
wellsnake <wellsunweizhen@gmail.com>
2013-03-27 21:01:39 +0800

    为系统增加了4个返回json数据的api 1./getcommon 获得普通号 2./getspecial 获得专病号 3./getexpert 获得专家号 4./getvip 获得特需号

--- getchhospital/main.py
+++ getchhospital/main.py
@@ -16,12 +16,80 @@
 #
 import webapp2
 from chmodel import clinic_schedule_common as comm
+from chmodel import clinic_schedule_special as sp
+from chmodel import clinic_schedule_expert as ex
+from chmodel import clinic_schedule_vip as vp
+import json
 
 
-class MainHandler(webapp2.RequestHandler):
+class index(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("<p><a href='/getcommon'>获得所有普通号</a></p>")
+        self.response.out.write("<p><a href='/getspecial'>获得所有专病号</a></p>")
+        self.response.out.write("<p><a href='/getexpert'>获得所有专家号</a></p>")
+        self.response.out.write("<p><a href='/getvip'>获得所有特需号</a></p>")
+
+
+class common(webapp2.RequestHandler):
     def get(self):
         q = comm.all()
         results = q.fetch(q.count())
+        a = []
         for r in results:
-            self.response.write(r.dept_name)
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+            a.append({'dept_name': r.dept_name, 'schedule_desc': r.schedule_desc})
+        x = {'count': q.count(), 'desc': a}
+        self.response.headers["Content-Type"] = "application/json; charset=utf-8"
+        self.response.out.write(json.dumps(x))
+
+class special(webapp2.RequestHandler):
+    def get(self):
+        q = sp.all()
+        results = q.fetch(q.count())
+        a = []
+        for r in results:
+            a.append({'dept_name': r.dept_name, 'clinic_name': r.clinic_name, 'schedule_desc':r.schedule_desc})
+        x = {'count': q.count(), 'desc': a}
+        self.response.headers["Content-Type"] = "application/json; charset=utf-8"
+        self.response.out.write(json.dumps(x))
+
+class expert(webapp2.RequestHandler):
+    def get(self):
+        q = ex.all()
+        results = q.fetch(q.count())
+        a = []
+        for r in results:
+            a.append({'dept_name': r.dept_name,
+                      'doctor_name': r.doctor_name,
+                      'doctor_level': r.doctor_level,
+                      'doctor_skill': r.doctor_skill,
+                      'clinic_desc': r.clinic_desc,
+                      'clinic_num': r.clinic_num,
+                      'week': r.week})
+        x = {'count': q.count(), 'desc': a}
+        self.response.headers["Content-Type"] = "application/json; charset=utf-8"
+        self.response.out.write(json.dumps(x))
+
+class vip(webapp2.RequestHandler):
+    def get(self):
+        q = vp.all()
+        results = q.fetch(q.count())
+        a = []
+        for r in results:
+            a.append({'dept_name': r.dept_name,
+                      'doctor_name': r.doctor_name,
+                      'doctor_level': r.doctor_level,
+                      'doctor_skill': r.doctor_skill,
+                      'clinic_desc': r.clinic_desc,
+                      'clinic_num': r.clinic_num,
+                      'week': r.week})
+        x = {'count': q.count(), 'desc': a}
+        self.response.headers["Content-Type"] = "application/json; charset=utf-8"
+        self.response.out.write(json.dumps(x))
+
+
+app = webapp2.WSGIApplication([('/', index)], debug=False)
+appcommon = webapp2.WSGIApplication([('/getcommon', common)], debug=False)
+appspecial = webapp2.WSGIApplication([('/getspecial', special)], debug=False)
+appexpert = webapp2.WSGIApplication([('/getexpert', expert)], debug=False)
+appvip = webapp2.WSGIApplication([('/getvip', vip)], debug=False)
+
